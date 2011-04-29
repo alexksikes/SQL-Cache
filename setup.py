f0664da3dc1bd520c8764af9cb75d14ca6be45fd
Alex Ksikes <alex.ksikes@gmail.com>
2011-04-29 08:14:20 -0700

    Register the module to PyPI

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ from distutils.core import setup
 
 setup(name='SQLCache',
     version='0.3',
-    description='A simple key-value MySQL store for small chunks of arbitrary data (strings, objects).',
+    description='SQLCache is loosely similar to memcached but using MySQL.',
     author='Alex Ksikes',
     author_email='alex.ksikes@gmail.com',
+    url='https://github.com/alexksikes/SQL-Cache',
     py_modules=['sql_cache'],
+    long_description='A simple key-value MySQL store for small chunks of arbitrary data (strings, objects).',
     license='GPL'
 )
